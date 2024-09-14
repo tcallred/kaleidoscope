99b45ec800fbceb6b911b004e1c0320deefaa96c
Taylor Allred <taylor.allred21@gmail.com>
2024-09-14 15:44:23 -0600

    rust: simplify lexer; add more token variants

--- kaleidoscope-rs/src/lexer.rs
+++ kaleidoscope-rs/src/lexer.rs
@@ -5,65 +5,58 @@ pub enum Token {
     Extern,
     Identifier(String),
     Number(f64),
+    LParen,
+    RParen,
+    Comma,
+    Semicolon,
     Other(char),
-}
-
-fn skip_whitespace(input: &[char], mut idx: usize) -> usize {
-    while idx < input.len() && input[idx].is_whitespace() {
-        idx += 1;
-    }
-    idx
-}
-
-fn lex_keyword_or_identifier(input: &[char], mut idx: usize, tokens: &mut Vec<Token>) -> usize {
-    let mut identifier_str = String::new();
-    while input[idx].is_alphanumeric() {
-        identifier_str.push(input[idx]);
-        idx += 1;
-    }
-    if identifier_str == "def" {
-        tokens.push(Token::Def);
-    } else if identifier_str == "extern" {
-        tokens.push(Token::Extern);
-    } else {
-        tokens.push(Token::Identifier(identifier_str));
-    }
-    idx
-}
-
-fn lex_number(input: &[char], mut idx: usize, tokens: &mut Vec<Token>) -> usize {
-    let mut num_str = String::new();
-    while input[idx].is_numeric() || input[idx] == '.' {
-        num_str.push(input[idx]);
-        idx += 1;
-    }
-    tokens.push(Token::Number(num_str.parse().unwrap()));
-    idx
-}
-
-fn skip_comment(input: &[char], mut idx: usize) -> usize {
-    while input[idx] != '\n' && input[idx] != '\r' {
-        idx += 1;
-    }
-    idx
 }
 
 pub fn lex(input: &[char]) -> Vec<Token> {
     let mut idx = 0;
     let mut tokens: Vec<Token> = vec![];
     while idx < input.len() {
-        idx = skip_whitespace(input, idx);
+        while idx < input.len() && input[idx].is_whitespace() {
+            idx += 1;
+        }
         if idx >= input.len() {
             break;
         }
         if input[idx].is_alphabetic() {
-            idx = lex_keyword_or_identifier(input, idx, &mut tokens)
+            let tokens: &mut Vec<Token> = &mut tokens;
+            let mut identifier_str = String::new();
+            while input[idx].is_alphanumeric() {
+                identifier_str.push(input[idx]);
+                idx += 1;
+            }
+            if identifier_str == "def" {
+                tokens.push(Token::Def);
+            } else if identifier_str == "extern" {
+                tokens.push(Token::Extern);
+            } else {
+                tokens.push(Token::Identifier(identifier_str));
+            }
         } else if input[idx].is_numeric() || input[idx] == '.' {
-            idx = lex_number(input, idx, &mut tokens)
+            let tokens: &mut Vec<Token> = &mut tokens;
+            let mut num_str = String::new();
+            while input[idx].is_numeric() || input[idx] == '.' {
+                num_str.push(input[idx]);
+                idx += 1;
+            }
+            tokens.push(Token::Number(num_str.parse().unwrap()));
         } else if input[idx] == '#' {
-            idx = skip_comment(input, idx)
+            while input[idx] != '\n' && input[idx] != '\r' {
+                idx += 1;
+            }
         } else {
-            tokens.push(Token::Other(input[idx]));
+            let tok = match input[idx] {
+                '(' => Token::LParen,
+                ')' => Token::RParen,
+                ',' => Token::Comma,
+                ';' => Token::Semicolon,
+                _ => Token::Other(input[idx]),
+            };
+            tokens.push(tok);
             idx += 1;
         }
     }
--- kaleidoscope-rs/src/parser.rs
+++ kaleidoscope-rs/src/parser.rs
@@ -60,31 +60,31 @@ fn parse_number(tokens: &[Token], idx: &mut usize) -> Result<ExprAST, ParseError
 }
 
 fn parse_paren_expr(tokens: &[Token], idx: &mut usize) -> Result<ExprAST, ParseError> {
-    expect(tokens, idx, Token::Other('('))?;
+    expect(tokens, idx, Token::LParen)?;
     let expr = parse_expression(tokens, idx)?;
-    expect(tokens, idx, Token::Other(')'))?;
+    expect(tokens, idx, Token::RParen)?;
     Ok(expr)
 }
 
 fn parse_identifier_expr(tokens: &[Token], idx: &mut usize) -> Result<ExprAST, ParseError> {
     let id = expect_id(tokens, idx)?;
-    if tokens[*idx] != Token::Other('(') {
+    if tokens[*idx] != Token::LParen {
         return Ok(ExprAST::Variable(id));
     }
-    expect(tokens, idx, Token::Other('('))?;
+    expect(tokens, idx, Token::LParen)?;
     let mut args: Vec<ExprAST> = Vec::new();
-    if tokens[*idx] == Token::Other(')') {
-        expect(tokens, idx, Token::Other(')'))?;
+    if tokens[*idx] == Token::RParen {
+        expect(tokens, idx, Token::RParen)?;
         return Ok(ExprAST::Call { callee: id, args });
     }
     loop {
         let expr = parse_expression(tokens, idx)?;
         args.push(expr);
-        if tokens[*idx] == Token::Other(',') {
-            expect(tokens, idx, Token::Other(','))?;
+        if tokens[*idx] == Token::Comma {
+            expect(tokens, idx, Token::Comma)?;
         }
-        if tokens[*idx] == Token::Other(')') {
-            expect(tokens, idx, Token::Other(')'))?;
+        if tokens[*idx] == Token::RParen {
+            expect(tokens, idx, Token::RParen)?;
             break;
         }
     }
@@ -95,7 +95,7 @@ fn parse_primary(tokens: &[Token], idx: &mut usize) -> Result<ExprAST, ParseErro
     match tokens[*idx] {
         Token::Identifier(_) => parse_identifier_expr(tokens, idx),
         Token::Number(_) => parse_number(tokens, idx),
-        Token::Other('(') => parse_paren_expr(tokens, idx),
+        Token::LParen => parse_paren_expr(tokens, idx),
         _ => Err(ParseError(format!("Unexpected token {:?}", tokens[*idx]))),
     }
 }
@@ -152,17 +152,20 @@ fn parse_expression(tokens: &[Token], idx: &mut usize) -> Result<ExprAST, ParseE
 
 fn parse_prototype(tokens: &[Token], idx: &mut usize) -> Result<PrototypeAST, ParseError> {
     let fn_name = expect_id(tokens, idx)?;
-    expect(tokens, idx, Token::Other('('))?;
+    expect(tokens, idx, Token::LParen)?;
     let mut arg_names = Vec::new();
     loop {
-        if tokens[*idx] == Token::Other(')') {
-            expect(tokens, idx, Token::Other(')'))?;
+        if tokens[*idx] == Token::RParen {
+            expect(tokens, idx, Token::RParen)?;
             break;
         }
         let arg_name = expect_id(tokens, idx)?;
         arg_names.push(arg_name);
     }
-    Ok(PrototypeAST { name: fn_name, args: arg_names })
+    Ok(PrototypeAST {
+        name: fn_name,
+        args: arg_names,
+    })
 }
 
 fn parse_definition(tokens: &[Token], idx: &mut usize) -> Result<FunctionAST, ParseError> {
@@ -179,7 +182,10 @@ fn parse_extern(tokens: &[Token], idx: &mut usize) -> Result<PrototypeAST, Parse
 
 fn parse_top_level_expr(tokens: &[Token], idx: &mut usize) -> Result<FunctionAST, ParseError> {
     let e = parse_expression(tokens, idx)?;
-    let proto = PrototypeAST { name: "__anon_expr".to_string(), args: vec![] };
+    let proto = PrototypeAST {
+        name: "__anon_expr".to_string(),
+        args: vec![],
+    };
     Ok(FunctionAST { proto, body: e })
 }
 
@@ -188,15 +194,20 @@ pub fn parse(tokens: &[Token]) -> Result<Vec<FunctionAST>, ParseError> {
     let mut fns = Vec::new();
     loop {
         match tokens[idx] {
-            Token::Eof => { return Ok(fns); }
-            Token::Other(';') => { idx += 1 }
+            Token::Eof => {
+                return Ok(fns);
+            }
+            Token::Semicolon => idx += 1,
             Token::Def => {
                 let d = parse_definition(tokens, &mut idx)?;
                 fns.push(d);
             }
             Token::Extern => {
                 let ext = parse_extern(tokens, &mut idx)?;
-                fns.push(FunctionAST { proto: ext, body: ExprAST::Number(0.0) });
+                fns.push(FunctionAST {
+                    proto: ext,
+                    body: ExprAST::Number(0.0),
+                });
             }
             _ => {
                 let top_level = parse_top_level_expr(tokens, &mut idx)?;
@@ -208,8 +219,8 @@ pub fn parse(tokens: &[Token]) -> Result<Vec<FunctionAST>, ParseError> {
 
 #[cfg(test)]
 mod tests {
-    use crate::lexer::lex;
     use super::*;
+    use crate::lexer::lex;
 
     #[test]
     fn test_parse_exp() {
